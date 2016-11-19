a78421abd7248480c71eba3023adfcc8f12c99dd
Kathy Lau <kathy@mit.edu>
2016-11-18 20:58:38 -0500

    flaskify

--- nytimes.py
+++ nytimes.py
@@ -1,7 +1,57 @@
+from flask import Flask, render_template, request, redirect, url_for, session
 import cookielib, urllib2
+app = Flask(__name__)
 
 cj = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+
+#root
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method!="POST":
+        return render_template("index.html")
+    else:
+        r1 = request.form["news1"]
+        r2 = request.form["news2"]
+        r3 = request.form["news3"]
+        r4 = request.form["news4"]
+        r5 = request.form["news5"]
+        r6 = request.form["news6"]
+        r7 = request.form["news7"]
+        r8 = request.form["news8"]
+        r9 = request.form["news9"]
+        r10 = request.form["news10"]
+        stories = [r1,r2,r3,r4,r5,r6,r7,r8,r9,r10]
+        print stories
+        stories = fetch(stories)
+        return render_template("stories.html", news=stories)
+
+
+def fetch(urls):
+    stopwords=['Advertisement', 'Continue reading the main story']
+    text = ''
+
+    for i in urls:
+        try:
+            htmlfile = opener.open(i)
+            htmltext = htmlfile.read();
+            for a in stopwords:
+                htmltext = htmltext.replace(a, '')
+            pos = htmltext.find('<p class="story-body-text story-content"')
+            title = htmltext.find('<title>')
+            end = htmltext.find('</title>')
+            endstory = htmltext.find('</p><footer class="story-footer story-content">')
+            text +=  '<h1>' + htmltext[title + 7:end] + '</h1>' + htmltext[pos: endstory] + '<br><br><br><br><br><br><br><br>'
+        except:
+            lol=''
+    return text
+
+
+if __name__=="__main__":
+    app.debug = True
+    app.secret_key = "dogs are qool"
+    app.run()
+
 
 url = ["http://www.nytimes.com/2016/11/16/opinion/obama-in-trumpland.html?ref=opinion&mtrref=www.nytimes.com&assetType=opinion&mtrref=www.nytimes.com&assetType=opinion&mtrref=www.nytimes.com&gwh=5C07CA6BDBD85775B2F0FE25F0E94B99&gwt=pay&assetType=opinion",
        "http://www.nytimes.com/2016/11/17/us/politics/democrats-house-senate.html?mtrref=www.nytimes.com&gwh=E4A8B623BA2D20B2323C1628B714E1F7&gwt=pay",
@@ -18,7 +68,6 @@ url = ["http://www.nytimes.com/2016/11/16/opinion/obama-in-trumpland.html?ref=op
        'http://www.nytimes.com/2016/11/17/technology/personaltech/how-not-to-overpay-on-black-friday-let-the-web-be-your-guide.html?ref=technology',
        'http://www.nytimes.com/2016/11/16/business/dealbook/snapchats-parent-files-for-a-stock-offering.html?ref=technology']
 
-stopwords=['Advertisement', 'Continue reading the main story']
 
 ##filterB = '''<div class="newsletter-signup'''
 ##filterE ='''
@@ -27,25 +76,3 @@ stopwords=['Advertisement', 'Continue reading the main story']
 ##        <li class="privacy"><a href="http://www.nytimes.com/privacy" target="_blank">Privacy Policy</a></li>
 ##    </ul><!-- close footer -->
 ##</div>'''
-
-text = ''
-
-for i in url:
-    try:
-        htmlfile = opener.open(i)
-        htmltext = htmlfile.read();
-        for a in stopwords:
-            htmltext = htmltext.replace(a, '')
-        pos = htmltext.find('<p class="story-body-text story-content"')
-        title = htmltext.find('<title>')
-        end = htmltext.find('</title>')
-        endstory = htmltext.find('</p><footer class="story-footer story-content">')
-        text +=  '<h1>' + htmltext[title + 7:end] + '</h1>' + htmltext[pos: endstory] + '<br><br><br><br><br><br><br><br>'
-    except:
-        lol=''
-    
-f = open('test.html', 'w')
-f.write(text)
-f.close()
-
-
